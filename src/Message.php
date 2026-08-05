78069332f4ae71b763247186cc6cfafce47c16ad
Ivan Antonov <antonowano@gmail.com>
2026-08-05 22:33:36 +0300

    Implement the message by test

---
+++ src/Message.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Antonowano\Chat;
+
+use DateTimeInterface;
+
+readonly class Message
+{
+    public function __construct(
+        private string $text,
+        private DateTimeInterface $createdAt,
+        private string $author,
+    ) {
+    }
+
+    public function __toString(): string
+    {
+        return sprintf(
+            '[%s] %s: %s',
+            $this->createdAt->format('H:i:s d.m.Y'),
+            $this->author,
+            $this->text
+        );
+    }
+}
