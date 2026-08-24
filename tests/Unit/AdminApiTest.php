621113b0c628aa3a7813a5b0a2ac7fd714c9e95e
Ivan Antonov <antonowano@gmail.com>
2026-08-24 17:30:43 +0300

    Reorganize the test for checking the availability of user and room registration

--- tests/Unit/AdminApiTest.php
+++ tests/Unit/AdminApiTest.php
@@ -17,39 +17,45 @@ beforeEach(function (): void {
 
 describe('User registration', function (): void {
     beforeEach(function (): void {
-        $request = new StubHttpRequest('POST', '/api/user/register', [], [
+        $this->request = new StubHttpRequest('POST', '/api/user/register', [], [
             'name' => 'Ivan',
         ]);
-        $this->response = sendRequestToApi($this->router, $request, createUser(role: Role::ADMIN));
-        $this->accessToken = $this->response->data()['accessToken'] ?? '';
     });
 
-    it('should return 201 Created', function (): void {
-        expect($this->response->statusCode())->toBe(HttpStatusCode::CREATED);
+    describe('For admin', function (): void {
+        beforeEach(function (): void {
+            $this->response = sendRequestToApi($this->router, $this->request, createUser(role: Role::ADMIN));
+            $this->accessToken = $this->response->data()['accessToken'] ?? '';
+        });
+
+        it('should return 201 Created', function (): void {
+            expect($this->response->statusCode())->toBe(HttpStatusCode::CREATED);
+        });
+
+        it('should return access token', function (): void {
+            expect($this->accessToken)->not()->toBeEmpty();
+        });
+
+        it('should add the user to the storage', function (): void {
+            $expectedUser = createUser(id: 1, name: 'Ivan', accessToken: $this->accessToken);
+            expect($this->userStorage->findByToken($this->accessToken))->toEqual($expectedUser);
+        });
     });
 
-    it('should return access token', function (): void {
-        expect($this->accessToken)->not()->toBeEmpty();
-    });
+    describe('For a regular user', function (): void {
+        beforeEach(function (): void {
+            $this->response = sendRequestToApi($this->router, $this->request);
+        });
 
-    it('should be accessible from the user storage', function (): void {
-        $expectedUser = createUser(id: 1, name: 'Ivan', accessToken: $this->accessToken);
-        expect($this->userStorage->findByToken($this->accessToken))->toEqual($expectedUser);
-    });
-});
+        it('should return 403 Forbidden', function (): void {
+            expect($this->response)
+                ->statusCode()->toBe(HttpStatusCode::FORBIDDEN)
+                ->data()->toHaveKey('error');
+        });
 
-describe('User registration by another user', function (): void {
-    beforeEach(function (): void {
-        $request = new StubHttpRequest('POST', '/api/user/register', [], [
-            'name' => 'Olga',
-        ]);
-        $this->response = sendRequestToApi($this->router, $request);
-    });
-
-    it('should return 403 Forbidden', function (): void {
-        expect($this->response)
-            ->statusCode()->toBe(HttpStatusCode::FORBIDDEN)
-            ->data()->toHaveKey('error');
+        it('should not add users to the storage', function (): void {
+            expect($this->userStorage->findAllById([1]))->toBeEmpty();
+        });
     });
 });
 
@@ -57,21 +63,43 @@ describe('Room registration', function (): void {
     beforeEach(function (): void {
         $this->user1 = $this->userStorage->create(createNewUser());
         $this->user2 = $this->userStorage->create(createNewUser());
-        $request = new StubHttpRequest('POST', '/api/room/register', [], [
+        $this->request = new StubHttpRequest('POST', '/api/room/register', [], [
             'memberIds' => [$this->user1->id(), $this->user2->id()],
         ]);
-        $this->response = sendRequestToApi($this->router, $request, createUser(role: Role::ADMIN));
     });
 
-    it('should return 201 Created', function (): void {
-        expect($this->response->statusCode())->toBe(HttpStatusCode::CREATED);
+    describe('For admin', function (): void {
+        beforeEach(function (): void {
+            $this->response = sendRequestToApi($this->router, $this->request, createUser(role: Role::ADMIN));
+        });
+
+        it('should return 201 Created', function (): void {
+            expect($this->response)
+                ->statusCode()->toBe(HttpStatusCode::CREATED);
+        });
+
+        it('should add the room to the storage', function (): void {
+            $expectedRoom = createRoom(
+                id: 1,
+                members: [$this->user1, $this->user2],
+            );
+            expect($this->roomStorage->findById(1))->toEqual($expectedRoom);
+        });
     });
 
-    it('should be accessible from the user storage', function (): void {
-        $expectedRoom = createRoom(
-            id: 1,
-            members: [$this->user1, $this->user2],
-        );
-        expect($this->roomStorage->findById(1))->toEqual($expectedRoom);
+    describe('For a regular user', function (): void {
+        beforeEach(function (): void {
+            $this->response = sendRequestToApi($this->router, $this->request);
+        });
+
+        it('should return 403 Forbidden', function (): void {
+            expect($this->response)
+                ->statusCode()->toBe(HttpStatusCode::FORBIDDEN)
+                ->data()->toHaveKey('error');
+        });
+
+        it('should not add rooms to the storage', function (): void {
+            expect($this->roomStorage->findById(1))->toBeNull();
+        });
     });
 });
