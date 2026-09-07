7730e6d0bcef62cfd15e2652f940e66aaf58a73d
Ivan Antonov <antonowano@gmail.com>
2026-09-07 22:21:13 +0300

    Test notifications of new rooms

--- tests/Unit/Api/RoomRegistrationTest.php
+++ tests/Unit/Api/RoomRegistrationTest.php
@@ -4,27 +4,44 @@ use Antonowano\Chat\Chat;
 use Antonowano\Chat\Enums\HttpStatusCode;
 use Antonowano\Chat\Role;
 use Antonowano\Chat\Stubs\StubHttpRequest;
+use Antonowano\Chat\Stubs\StubWsResponse;
+use Antonowano\Chat\Swoole\SwooleWsChatListener;
 use Symfony\Component\Clock\MockClock;
 
 beforeEach(function (): void {
     $this->chat = new Chat(new MockClock());
+    $this->events = $this->chat->events();
     $this->userStorage = $this->chat->userStorage();
     $this->roomStorage = $this->chat->roomStorage();
     $this->router = $this->chat->apiRouter();
-    $user1 = $this->userStorage->create(createNewUser());
-    $user2 = $this->userStorage->create(createNewUser());
+    $this->user1 = $this->userStorage->create(createNewUser('Ivan'));
+    $this->user2 = $this->userStorage->create(createNewUser('Olga'));
+    $this->user3 = $this->userStorage->create(createNewUser());
+    $this->userListener1 = new StubWsResponse();
+    $this->userListener2 = new StubWsResponse();
+    $this->userListener3 = new StubWsResponse();
+    $this->events->addListener('listener1', new SwooleWsChatListener($this->userListener1, $this->user1));
+    $this->events->addListener('listener2', new SwooleWsChatListener($this->userListener2, $this->user2));
+    $this->events->addListener('listener3', new SwooleWsChatListener($this->userListener3, $this->user3));
     $this->request = new StubHttpRequest('POST', '/api/room/register', [], [
-        'memberIds' => [$user1->id(), $user2->id()],
+        'memberIds' => [$this->user1->id(), $this->user2->id()],
     ]);
-    $this->registeredRoom = createRoom(
-        id: 1,
-        members: [$user1, $user2],
-    );
 });
 
 it('should return 201 Created', function (): void {
     $response = sendRequestToApi($this->router, $this->request, createUser(role: Role::ADMIN));
     expect($response)->statusCode()->toBe(HttpStatusCode::CREATED);
+});
+
+it('should notify listeners of a new message', function (): void {
+    sendRequestToApi($this->router, $this->request, createUser(role: Role::ADMIN));
+    expect($this->userListener1)->data()
+        ->toHaveKey('type', 'Room')
+        ->toHaveKey('data.id', 1)
+        ->toHaveKey('data.members.0.name', 'Ivan')
+        ->toHaveKey('data.members.1.name', 'Olga')
+        ->and($this->userListener2)->data()->toBe($this->userListener1->data())
+        ->and($this->userListener3)->data()->toBe([]);
 });
 
 it('should return 403 Forbidden for not an admin', function (): void {
@@ -34,5 +51,9 @@ it('should return 403 Forbidden for not an admin', function (): void {
 
 it('should add the room to the storage', function (): void {
     sendRequestToApi($this->router, $this->request, createUser(role: Role::ADMIN));
-    expect($this->roomStorage)->findById(1)->toEqual($this->registeredRoom);
+    expect($this->roomStorage->findById(1))->not->toBeNull()
+        ->id()->toBe(1)
+        ->hasMember($this->user1)->toBeTrue()
+        ->hasMember($this->user2)->toBeTrue()
+        ->hasMember($this->user3)->toBeFalse();
 });
